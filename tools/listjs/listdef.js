b3094b2769b1c2ce6f808de71e34832e81e254c1
Brandon Curtis <brandon.curtis@gmail.com>
2015-07-13 00:33:36 -0700

    drafting list sorting and filtering

---
+++ tools/listjs/listdef.js
@@ -0,0 +1,99 @@
+var options = {
+  valueNames: [ 'title', 'format', 'category', 'description' ]
+};
+
+var contentList = new List('content-list', options);
+
+$('#filter-all').click(function() {
+  contentList.filter();
+  return false;
+});
+
+$('#filter-wiki').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().format == "Wiki") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
+
+$('#filter-drive').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().format == "Google Drive") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
+
+$('#filter-blog').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().format == "Blog") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
+
+$('#filter-app').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().format == "App") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
+
+$('#filter-finance').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().category == "Finance") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
+
+$('#filter-education').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().category == "Education") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
+
+
+$('#filter-technology').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().category == "Technology") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
+
+$('#filter-science').click(function() {
+  contentList.filter(function(item) {
+    if (item.values().category == "Science") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return false;
+});
