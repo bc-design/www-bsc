ba0b4d618fab272c85a64cec5be89ae9bdd1fcee
Brandon Curtis <brandon.curtis@gmail.com>
2015-08-05 20:56:53 -0700

    fixing search reset

--- javascript/listdef.js
+++ javascript/listdef.js
@@ -6,7 +6,7 @@ var contentList = new List('content-list', options);
 
 $('#filter-all').click(function() {
   contentList.filter();
-  contentList.search.reset();
+  contentList.reset();
   return false;
 });
 
